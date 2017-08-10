e41da5b77893c94a3c3bbcc6a2459fe4e0ac48fe
ashsharma7 <ash_sharma@hotmail.com>
2017-08-10 17:25:26 +0530

    CRUD done. Jsonify done

--- vagrant/project.py
+++ vagrant/project.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, url_for, redirect, flash, jsonify
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
@@ -10,23 +10,11 @@ session = DBSession()
 
 app = Flask(__name__)
 
-
 @app.route('/')
-@app.route('/hello')
-def helloWorld():
+@app.route('/restaurants/')
+def listRestaurants():
     restaurants = session.query(Restaurant)
-    output = ""
-    for r in restaurants:
-        output += "<strong>"
-        output += r.name
-        output += "</strong>"
-        output += "<br>"
-        items = session.query(MenuItem).filter_by(restaurant_id = r.id)
-        for i in items:
-            output += i.name + " " + i.price + "<br>&#160;&#160;&#160;&#160;" + i.description
-            output += "<br>"
-        output += "<br><br>"
-    return output
+    return render_template("restaurants.html", restaurants=restaurants)
 
 @app.route('/restaurants/<int:restaurant_id>/')
 def restaurantMenu(restaurant_id):
@@ -39,9 +27,14 @@ def restaurantMenu(restaurant_id):
 @app.route('/restaurants/<int:restaurant_id>/new/', methods=['GET','POST'])
 def newMenuItem(restaurant_id):
     if request.method == 'POST':
-        newItem = MenuItem(name = request.form['name'], restaurant_id = restaurant_id)
+        newItem = MenuItem(name = request.form['name'], 
+            description=request.form['description'],
+            price = request.form['price'],
+            course = request.form['course'],
+            restaurant_id = restaurant_id)
         session.add(newItem)
         session.commit()
+        flash('New Menu Item Created!')
         return redirect(url_for("restaurantMenu",restaurant_id=restaurant_id))
     else:
         return render_template("newmenuitem.html",restaurant_id = restaurant_id)
@@ -55,6 +48,7 @@ def editMenuItem(restaurant_id, menu_id):
         itemToEdit.name = newName
         session.add(itemToEdit)
         session.commit()
+        flash('Menu Item Edited!')
         return redirect(url_for("restaurantMenu",restaurant_id=restaurant_id))
     else:
         r = session.query(Restaurant).filter_by(id=restaurant_id).one()
@@ -69,11 +63,20 @@ def deleteMenuItem(restaurant_id, menu_id):
         itemToDelete = session.query(MenuItem).filter_by(restaurant_id=restaurant_id, id = menu_id).one()
         session.delete(itemToDelete)
         session.commit()
+        flash('Menu Item Deleted!')
         return redirect(url_for("restaurantMenu",restaurant_id=restaurant_id))
     else:
         itemToDelete = session.query(MenuItem).filter_by(restaurant_id=restaurant_id, id = menu_id).one()
         return render_template("deletemenuitem.html",item=itemToDelete)
 
+@app.route('/restaurants/<int:restaurant_id>/menu/JSON')
+def restaurantMenuJSON(restaurant_id):
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    items = session.query(MenuItem).filter_by(
+        restaurant_id=restaurant_id).all()
+    return jsonify(MenuItems=[i.serialize for i in items])
+
 if __name__ == '__main__':
+    app.secret_key = 'SuperSecretKey'
     app.debug = True
     app.run(host = '0.0.0.0', port = 5000)
